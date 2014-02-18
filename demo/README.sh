b5a508f916027de527c1a6700303a21c083de81d
pellinbroca <pellinbroca@outlook.com>
2014-02-18 09:40:23 +0100

    Add notice to docs that only sha1 is currently supported

--- demo/README.sh
+++ demo/README.sh
@@ -5,7 +5,7 @@ openssl genrsa -out key.pem 4096
 # Public certificate generated with:
 openssl req -nodes -new -x509 -key key.pem -out cert.pem -days 365
 
-# Signature generated with:
+# Signature generated with (currently only sha1 is supported):
 openssl dgst -sign key.pem -sha1 server/main.js | xxd -p > server/main.js.sig
 
 # See local/example.html
